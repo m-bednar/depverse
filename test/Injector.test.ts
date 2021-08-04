96c052665e596380a81574924eef8a84cf6d35a2
mar-bednar <mbedna8@seznam.cz>
2021-08-04 22:37:28 +0200

    Injector class and test template added

---
+++ test/Injector.test.ts
@@ -0,0 +1,52 @@
+
+describe('Injector', () => {
+
+   describe('construct()', () => {
+
+      context('when all dependencies exists', () => {
+
+         it('should return instance with all injected dependencies', () => {
+            throw Error('Not implemented');
+         }); 
+
+         it('should pass arguments to the constructor', () => {
+            throw Error('Not implemented');
+         }); 
+   
+      });
+
+      context('when dependency is missing', () => {
+
+         it('should throw', () => {
+            throw Error('Not implemented');
+         }); 
+   
+      });
+
+   });
+
+   describe('inject()', () => {
+
+      context('when all dependencies exists', () => {
+
+         it('should inject all dependencies', () => {
+            throw Error('Not implemented');
+         }); 
+
+         it('should return same instance as passed', () => {
+            throw Error('Not implemented');
+         }); 
+
+      });
+
+      context('when dependency is missing', () => {
+
+         it('should throw', () => {
+            throw Error('Not implemented');
+         }); 
+   
+      });
+
+   });
+
+});
