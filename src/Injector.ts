f632c171a0b1b134392131561880286cb5a5b546
mar-bednar <mbedna8@seznam.cz>
2021-08-05 15:05:21 +0200

    Injector updated

--- src/Injector.ts
+++ src/Injector.ts
@@ -2,23 +2,26 @@ import { Class } from './types/Class';
 
 export class Injector {
 
-   /**
-    * Constructs new instance from given class with all dependencies already injected.
-    * @param cls Class to be constructed.
-    * @param constructorArgs Arguments, that will be passed to constructor.
-    * @returns Instance of passed class with all dependencies injected.
-    */
-   construct<T>(cls: Class<T>, ...constructorArgs: any): T {
-      return new cls(...constructorArgs);
-   }
+    public static readonly instance = new Injector();
 
-   /**
-    * Injects dependencies into given instance.
-    * @param instance Instance, where dependencies will be injected. 
-    * @returns Same instance as passed, but with injected dependencies.
-    */
-   inject<T>(instance: T): T {
-      return instance;
-   }
+    /**
+     * Constructs new instance from given class with all dependencies already injected.
+     * @param cls Class to be constructed.
+     * @param constructorArgs Arguments, that will be passed to constructor.
+     * @returns Instance of passed class with all dependencies injected.
+     */
+    public construct<T>(cls: Class<T>, ...constructorArgs: any): T {
+        return new cls(...constructorArgs);
+    }
 
+    /**
+     * Injects dependencies into given instance.
+     * @param instance Instance, where dependencies will be injected.
+     * @returns Same instance as passed, but with injected dependencies.
+     */
+    public inject<T>(instance: T): T {
+        const blacklistedTypes = ['Object', 'Function', 'String', 'Number', 'Symbol', 'BigInt'];
+        return instance;
+    }
+    
 }
--- test/Injector.test.ts
+++ test/Injector.test.ts
@@ -1,52 +1,45 @@
+import { Injector } from '../src';
 
 describe('Injector', () => {
+    describe('construct()', () => {
+        context('when all dependencies exists', () => {
+            let injector: Injector;
 
-   describe('construct()', () => {
+            beforeEach(() => {
+                injector = new Injector();
+            });
 
-      context('when all dependencies exists', () => {
+            it('should return instance with all injected dependencies', () => {
+                throw Error('Not implemented');
+            });
 
-         it('should return instance with all injected dependencies', () => {
-            throw Error('Not implemented');
-         }); 
+            it('should pass arguments to the constructor', () => {
+                throw Error('Not implemented');
+            });
+        });
 
-         it('should pass arguments to the constructor', () => {
-            throw Error('Not implemented');
-         }); 
-   
-      });
+        context('when dependency is missing', () => {
+            it('should throw dependency error', () => {
+                throw Error('Not implemented');
+            });
+        });
+    });
 
-      context('when dependency is missing', () => {
+    describe('inject()', () => {
+        context('when all dependencies exists', () => {
+            it('should inject all dependencies', () => {
+                throw Error('Not implemented');
+            });
 
-         it('should throw', () => {
-            throw Error('Not implemented');
-         }); 
-   
-      });
+            it('should return same instance as passed', () => {
+                throw Error('Not implemented');
+            });
+        });
 
-   });
-
-   describe('inject()', () => {
-
-      context('when all dependencies exists', () => {
-
-         it('should inject all dependencies', () => {
-            throw Error('Not implemented');
-         }); 
-
-         it('should return same instance as passed', () => {
-            throw Error('Not implemented');
-         }); 
-
-      });
-
-      context('when dependency is missing', () => {
-
-         it('should throw', () => {
-            throw Error('Not implemented');
-         }); 
-   
-      });
-
-   });
-
+        context('when dependency is missing', () => {
+            it('should throw dependency error', () => {
+                throw Error('Not implemented');
+            });
+        });
+    });
 });
