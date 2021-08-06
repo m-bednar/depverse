a253a1611c465c1dd972e4f514e625073db22a5d
mar-bednar <mbedna8@seznam.cz>
2021-08-06 17:47:35 +0200

    Type alias implemented

--- src/decorators/Inject.ts
+++ src/decorators/Inject.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
 import { MetadataKeys } from '../MetadataKeys';
+import { Class } from '../types/Class';
 
 /**
  * Property decorator marking property, which value should be injected by Injector.
+ * @param typeAlias Type used as injecting type instead of actual type of property.
  * @returns Decorator function.
  */
-export function Inject() {
+export function Inject(typeAlias?: Class) {
     return (target: Record<PropertyKey, any>, key: string) => {
-        const propType = Reflect.getMetadata('design:type', target, key);
+        const propType = typeAlias ?? Reflect.getMetadata('design:type', target, key);
         const metadata = Reflect.getMetadata(MetadataKeys.Inject, target.constructor) || [];
         metadata.push({ key, propType });
         Reflect.defineMetadata(MetadataKeys.Inject, metadata, target.constructor);
--- test/decorators/Inject.test.ts
+++ test/decorators/Inject.test.ts
@@ -15,23 +15,28 @@ describe('Inject', () => {
             @Inject()
             private prop2!: TestClass;
 
+            @Inject(TestClass)
+            private prop3!: any;
+
         }
 
         it('should set metadata by all used decorators', () => {
             const metadata = Reflect.getMetadata(MetadataKeys.Inject, TestClass);
-            expect(metadata).to.be.an('array').and.to.have.lengthOf(2);
+            expect(metadata).to.be.an('array').and.to.have.lengthOf(3);
         });
 
         it('should store keys in metadata', () => {
             const metadata = Reflect.getMetadata(MetadataKeys.Inject, TestClass);
             expect(metadata[0]).to.be.an('object').and.to.have.property('key', 'prop1');
             expect(metadata[1]).to.be.an('object').and.to.have.property('key', 'prop2');
+            expect(metadata[2]).to.be.an('object').and.to.have.property('key', 'prop3');
         });
 
         it('should store property types in metadata', () => {
             const metadata = Reflect.getMetadata(MetadataKeys.Inject, TestClass);
             expect(metadata[0]).to.be.an('object').and.to.have.property('propType', String);
             expect(metadata[1]).to.be.an('object').and.to.have.property('propType', TestClass);
+            expect(metadata[2]).to.be.an('object').and.to.have.property('propType', TestClass);
         });
 
     })
