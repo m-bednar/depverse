02d2220aa44d36b049f1655db6f0351c2f1dcdb0
mar-bednar <mbedna8@seznam.cz>
2021-08-09 19:52:20 +0200

    Exports added

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { Injector } from './Injector';
 export { Inject } from './decorators/Inject';
 export { Injectable } from './decorators/Injectable';
+export { AutoInject } from './decorators/AutoInject';
+export { InjectableMock } from './decorators/InjectableMock';
